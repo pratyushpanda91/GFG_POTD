9044c09c008dcac92ff85c7227ac193b8552874d
Pratyush Panda <126601890+pratyushpanda91@users.noreply.github.com>
2025-02-11 21:57:45 +0530

    11/02

---
+++ 2025 Monthwise/2. February/11_Check for BST.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    // Function to check whether a Binary Tree is BST or not.
+    bool isBST(Node* root) {
+        // Your code here
+        return isBST(root, INT_MIN, INT_MAX);
+    }
+    bool isBST(Node *root, long min_val , long max_val)
+    {
+        if(root==NULL)
+        return true;
+        if(root->data>=max_val || root->data<=min_val)
+        return false;
+        return isBST(root->left , min_val , root->data)
+        && isBST(root->right , root->data, max_val);
+    }
+};
