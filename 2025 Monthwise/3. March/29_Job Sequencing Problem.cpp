effe5df2a04d410a02f8884026eba6a35a919672
Pratyush Panda <126601890+pratyushpanda91@users.noreply.github.com>
2025-03-29 16:23:47 +0530

    Create 29_Job Sequencing Problem.cpp

---
+++ 2025 Monthwise/3. March/29_Job Sequencing Problem.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+    vector<int> jobSequencing(vector<int> &arr, vector<int> &profit) {
+        int n = arr.size();
+        vector<pair<int, int>> p;
+        int maxi = 0;
+
+        for (int i = 0; i < n; i++) {
+            p.push_back({profit[i], arr[i]}); // Corrected access
+            maxi = max(maxi, arr[i]);
+        }
+
+        sort(p.rbegin(), p.rend());
+        set<int> s;
+        for (int i = 1; i <= maxi; i++) {
+            s.insert(i);
+        }
+
+        int sum = 0, cnt = 0;
+
+        for (int i = 0; i < p.size(); i++) {
+            if (s.empty()) break;
+            auto it = s.upper_bound(p[i].second);
+
+            if (it != s.begin()) {
+                it--;
+                sum += p[i].first;
+                cnt++;
+                s.erase(it);
+            }
+        }
+
+        return {cnt, sum};
+    }
+};
