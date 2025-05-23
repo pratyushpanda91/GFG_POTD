8a82351bc45491c9e31bf054474697b0484b9be2
Pratyush Panda <126601890+pratyushpanda91@users.noreply.github.com>
2025-05-23 14:43:33 +0530

    Create 23_Dice throw.cpp

---
+++ 2025 Monthwise/May/23_Dice throw.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int noOfWays(int m, int n, int x) {
+        vector<vector<int>> dp(n + 1, vector<int>(x + 1, 0));
+        dp[0][0] = 1; // base case
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= x; ++j) {
+                for (int f = 1; f <= m; ++f) {
+                    if (j - f >= 0) {
+                        dp[i][j] += dp[i - 1][j - f];
+                    }
+                }
+            }
+        }
+
+        return dp[n][x];
+    }
+};
